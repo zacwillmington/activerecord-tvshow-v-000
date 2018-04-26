251e1d0f382d516dfb2894a4d55b869a01b5e35c
Zac Willmington <zac.willmington@gmail.com>
2018-04-26 16:00:35 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,3 +1,9 @@
 class CreateShows < ActiveRecord::migration
-
+def change
+    create_table :shows do |t|
+        t.string :name
+        t.string :day
+        t.string :network
+        t.integer :rating
+    end
 end
