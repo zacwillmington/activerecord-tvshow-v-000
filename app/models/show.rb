a1968b166da80cb20b12862e4bd9c214df51cd0a
Zac Willmington <zac.willmington@gmail.com>
2018-04-26 16:44:26 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,8 @@ class Show < ActiveRecord::Base
     def self.highest_rating
         Show.maximum('rating')
     end
+
+    def self.most_popular_show
+        Show.where(maximum('rating')).first
+    end
 end
