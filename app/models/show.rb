de4acc204929c0a81cb2558313df92f0f3c76e72
Zac Willmington <zac.willmington@gmail.com>
2018-04-26 16:55:58 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -22,4 +22,8 @@ class Show < ActiveRecord::Base
     def self.popular_shows
         Show.where('rating > 5')
     end
+
+    def self.shows_by_alphabetical_order
+        Show.order('title' DESC)
+    end
 end
